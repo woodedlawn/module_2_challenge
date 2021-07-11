72e28cf8fd6ec58da194fc3c5aab820b1527dd61
woodedlawn <jonathan@woodedlawn.com>
2021-07-10 20:40:39 -0700

    add save_csv function

--- qualifier/utils/fileio.py
+++ qualifier/utils/fileio.py
@@ -28,3 +28,27 @@ def load_csv(csvpath):
         for row in csvreader:
             data.append(row)
     return data
+
+
+
+def save_csv(csvpath, csvdata):
+    """Saves the data as a CSV file to path provided.
+
+    Args:
+        csvpath (Path): The csv file path.
+        csvdata (list of lists): The csv data to save
+
+    Returns:
+        None
+
+    """
+    with open(csvpath, "w") as csvfile:
+        csvwriter = csv.writer(csvfile)
+
+        # add the CSV header
+        csvwriter.writerow(["Lender","Max Loan Amount","Max LTV","Max DTI","Min Credit Score","Interest Rate"])
+
+        # write the CSV data
+        for row in csvdata:
+            csvwriter.writerow(row)
+    return None
